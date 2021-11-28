d3a09f171f8e6ee08779a9aa034fc94e21eacfd3
KIM DONG IL <cappu159@gmail.com>
2021-11-28 17:49:14 +0900

    slice setting

---
+++ src/redux/slices/gallerySlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const gallerySlice = createSlice({
+  name: "gallery",
+  initialState: {
+    photos: [],
+    isLoading: false,
+  },
+});
+
+export default gallerySlice.reducer;
